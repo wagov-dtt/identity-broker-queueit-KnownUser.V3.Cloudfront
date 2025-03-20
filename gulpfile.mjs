a8f6ff0a0721ff86514a0899e250a839f51059a2
Adel Amara <aamara@integral.com.au>
2025-03-20 14:39:44 +0100

    fix gulp

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,10 +1,11 @@
-const {src, dest, series} = require("gulp");
-const zip = require("gulp-zip");
-const modifyFile = require('gulp-modify-file');
-const jsonModify = require('gulp-json-modify')
-const path = require('path')
+import {src, dest, series} from "gulp";
+import zip from "gulp-zip";
+import modifyFile from "gulp-modify-file";
+import jsonModify from "gulp-json-modify";
+import path from "path";
 
 function zipRequest() {
+    console.log("Zip request...");
     return src([
         './ViewerRequest/**/*.js',
         '!./ViewerRequest/node_modules/**',
@@ -15,6 +16,7 @@ function zipRequest() {
 }
 
 function zipResponse() {
+    console.log("Zip response...");
     return src([
         './ViewerResponse/**/*.js',
         '!./ViewerResponse/node_modules/**',
@@ -24,20 +26,19 @@ function zipResponse() {
         .pipe(dest('./dist'));
 }
 
-function prepare() {
+export function prepare() {
+    console.log("Prepare...");
     return src([
         'ViewerRequest/sdk/*.js'
     ])
         .pipe(dest('dist/ViewerRequest/sdk'));
 }
 
-exports.stripPackage = () => {
+export function stripPackage(){
     return src(['./package.json'])
         .pipe(jsonModify({key: 'devDependencies', value: {}}))
         .pipe(jsonModify({key: 'scripts', value: {}}))
         .pipe(dest('./'))
 }
 
-exports.prepare = prepare;
-exports.buildArtifacts = series(prepare, zipRequest, zipResponse);
-exports.default = exports.buildArtifacts;
+export default series(prepare, zipRequest, zipResponse);
